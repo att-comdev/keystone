cdf19c5cb52a8d6ccec84b4041796f4854f91cf5
Alan Meadows <alan.meadows@gmail.com>
2017-01-17 12:16:19 -0600

    Add demo change for container release

--- keystone/notifications.py
+++ keystone/notifications.py
@@ -12,6 +12,8 @@
 #   License for the specific language governing permissions and limitations
 #   under the License.
 
+# This is an AT&T demo release of a new keystone commit
+
 """Notifications module for OpenStack Identity Service resources."""
 
 import collections
